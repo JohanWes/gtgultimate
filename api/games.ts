4fce7a81d80827a25bc2d2e74b0a23f8ca79756d
JohanWes <johan_evan@hotmail.com>
2025-12-07 14:51:25 +0100

    refactor(api): move lib locally to api/_lib to fix vercel module resolution

--- api/games.ts
+++ api/games.ts
@@ -1,5 +1,5 @@
 
-import clientPromise from '../lib/mongodb.js';
+import clientPromise from './_lib/mongodb.js';
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') {
